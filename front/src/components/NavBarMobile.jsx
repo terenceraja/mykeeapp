568c84fde039a341a8f18ffe11ef6dfafc06e90a
Terence Rajapaksage <terenceraja@hotmail.fr>
2024-01-23 22:57:04 +0100

    responsive test

--- front/src/components/NavBarMobile.jsx
+++ front/src/components/NavBarMobile.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { NavBarData } from "../data/NavBarData";
+import { Link } from "react-router-dom";
+
+import styles from "../styles/components/NavBarMobile.module.css";
 
 const NavBarMobile = () => {
-  return <div>NavBarMobile</div>;
+  const navList = NavBarData.map((item, key) => {
+    return (
+      <li key={key}>
+        <Link to={item.path}>
+          <img src={item.icon} className={styles.icons} alt="navLogo" />
+        </Link>
+      </li>
+    );
+  });
+
+  return (
+    <nav className={styles.nav_Bar}>
+      <ul className={styles.nav_rows_container}>{navList}</ul>
+    </nav>
+  );
 };
 
 export default NavBarMobile;
--- front/src/pages/Layout.jsx
+++ front/src/pages/Layout.jsx
@@ -1,19 +1,35 @@
 import React from "react";
-
+import { useMediaQuery } from "react-responsive";
 import { Outlet } from "react-router-dom";
 import NavBar from "../components/NavBar";
+import NavBarMobile from "../components/NavBarMobile";
 
 import "../styles/index.css";
 
 const Layout = () => {
-  return (
-    <main className="main">
-      <NavBar />
-      <div className="outlet">
-        <Outlet />
-      </div>
-    </main>
-  );
+  const isMobile = useMediaQuery({
+    query: "(max-width: 745px)",
+  });
+
+  if (isMobile) {
+    return (
+      <main className="main">
+        <div className="outlet">
+          <Outlet />
+        </div>
+        {isMobile ? <NavBarMobile /> : <NavBar />}
+      </main>
+    );
+  } else {
+    return (
+      <main className="main">
+        {isMobile ? <NavBarMobile /> : <NavBar />}
+        <div className="outlet">
+          <Outlet />
+        </div>
+      </main>
+    );
+  }
 };
 
 export default Layout;
